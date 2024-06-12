5bbf5a3ca5e0900d49aeea9dd23db35824935800
muhammadfikri4 <unicornhorse1@gmail.com>
2024-06-12 20:09:37 +0700

    feat: create cloudinary env

--- src/app/struktural/strukturalConfig.ts
+++ src/app/struktural/strukturalConfig.ts
@@ -1,11 +1,12 @@
 import { v2 as cloudinary } from 'cloudinary';
 import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { ENV } from '../../libs';
 
 cloudinary.config({
-    cloud_name: 'dgciwba8m',
-    api_key: "872533227445899",
-    api_secret: "X5qKOS7MnFHDsfNs3jmMKl9wCw8"
+    cloud_name: ENV.CLOUDINARY_CLOUD_NAME,
+    api_key: ENV.CLOUDINARY_API_KEY,
+    api_secret: ENV.CLOUDINARY_API_SECRET
 });
 
 // Konfigurasi Multer-Cloudinary Storage
