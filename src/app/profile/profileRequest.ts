201ee37eb42595c111601636cbc6b6c976ea1fb9
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-07-25 22:32:49 +0700

    fix: optional body update profile

--- src/app/profile/profileRequest.ts
+++ src/app/profile/profileRequest.ts
@@ -5,11 +5,11 @@ export const updateProfileSchema = Joi.object({
     name: Joi.string().optional().messages({
         "any.string": MESSAGES.ERROR.INVALID.NAME,
     }),
-    nim: Joi.number().max(99999999999999).messages({
+    nim: Joi.number().optional().max(99999999999999).messages({
         "any.number": MESSAGES.ERROR.INVALID.NIM.FORMAT,
         "number.max": MESSAGES.ERROR.INVALID.NIM.LENGTH
     }),
-    email: Joi.string().email().messages({
+    email: Joi.string().optional().email().messages({
         "string.email": MESSAGES.ERROR.INVALID.GLOBAL.EMAIL
     }),
     instagram: Joi.string().optional(),
