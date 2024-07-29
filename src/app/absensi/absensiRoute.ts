e64c5b2dec350a05d17d2e76c15c3bc8e42ed64c
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-07-29 23:31:36 +0700

    add middleware validate request

--- src/app/absensi/absensiRoute.ts
+++ src/app/absensi/absensiRoute.ts
@@ -1,13 +1,16 @@
+import { imageSchema } from "app/global/imageRequest";
 import { Router } from "express";
+import { validateRequest } from "middleware/validateRequest";
 import { VerifyToken } from "../../middleware/verifyToken";
 import { CatchWrapper } from "../../utils/CatchWrapper";
 import { upload } from "./absensiConfig";
 import { createAbsensiAcaraController, createAbsensiSubAcaraController, getAbsensiController } from "./absensiController";
+import { createAbsensiAcaraSchema, createAbsensiSubAcaraSchema } from "./absensiRequest";
 
 const route = Router()
 
-route.post("/acara", VerifyToken, upload.single("image"), CatchWrapper(createAbsensiAcaraController))
-route.post("/sub-acara", VerifyToken, upload.single("image"), CatchWrapper(createAbsensiSubAcaraController))
+route.post("/acara", VerifyToken, validateRequest(createAbsensiAcaraSchema, imageSchema), upload.single("image"), CatchWrapper(createAbsensiAcaraController))
+route.post("/sub-acara", VerifyToken, validateRequest(createAbsensiSubAcaraSchema, imageSchema), upload.single("image"), CatchWrapper(createAbsensiSubAcaraController))
 route.get("/", VerifyToken, CatchWrapper(getAbsensiController))
 
 export default route
