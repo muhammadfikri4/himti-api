e46002b7d8c894af610084bdac61939beaac98d8
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-08-10 02:12:26 +0700

    fix: middleware token

--- src/app/authentication/authRoute.ts
+++ src/app/authentication/authRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { validateRequest } from "../../middleware/validateRequest";
+import { VerifyToken } from "../../middleware/verifyToken";
 import { CatchWrapper } from "../../utils/CatchWrapper";
 import { forgotPasswordController, loginAdminController, loginController, logoutController, registerController, requestOtpController, validateOtpController } from "./authController";
 import { forgotPasswordSchema, loginSchema, registerSchema, requestOtpSchema, validateOtpSchema } from "./authRequest";
@@ -12,6 +13,6 @@ route.post("/admin/login", validateRequest(loginSchema), CatchWrapper(loginAdmin
 route.post('/otp', validateRequest(requestOtpSchema), CatchWrapper(requestOtpController))
 route.post('/otp/validate', validateRequest(validateOtpSchema), CatchWrapper(validateOtpController))
 route.put('/forgot-password', validateRequest(forgotPasswordSchema), CatchWrapper(forgotPasswordController))
-route.get('/logout', CatchWrapper(logoutController))
+route.get('/logout', VerifyToken, CatchWrapper(logoutController))
 
 export default route
