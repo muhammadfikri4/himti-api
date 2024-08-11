ba509dadc7e6bad5026edaa8df4c041847fcef22
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-08-11 21:47:01 +0700

    fix: comment parse json

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use(cookieParser())
 // app.use(bodyParser.json())
-app.use(express.json())
+// app.use(express.json())
 app.use(routes);
 app.use(HandlingError)
 
