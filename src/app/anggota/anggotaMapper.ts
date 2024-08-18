6f1f4a5dbe5fbe96228b48dd5099bcde9fe43070
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-08-18 20:23:24 +0700

    fix: logic is active on anggota mapper

--- src/app/anggota/anggotaMapper.ts
+++ src/app/anggota/anggotaMapper.ts
@@ -2,9 +2,9 @@ import { AnggotaModelTypes } from "./anggotaTypes";
 
 export const anggotaMapper = async (anggotas: AnggotaModelTypes[]) => {
     const mapper = await Promise.all(anggotas.map(async (anggota: AnggotaModelTypes) => {
-        const { id, createdAt, updatedAt, email, isActive, name, nim, facebook, instagram, linkedin, twitter } = anggota
+        const { id, createdAt, updatedAt, email, name, nim, facebook, instagram, linkedin, twitter } = anggota
         const sosmed = { facebook, instagram, linkedin, twitter }
-
+        const now = new Date().getFullYear()
         return {
             id,
             nim,
@@ -15,7 +15,7 @@ export const anggotaMapper = async (anggotas: AnggotaModelTypes[]) => {
                 year: anggota.angkatan.year
             },
             ...sosmed,
-            isActive,
+            isActive: Number(anggota.angkatan.year) <= now - 5 ? true : false,
             createdAt,
             updatedAt
 
