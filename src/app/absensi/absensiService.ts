8305e36629acefe843125fde32bbb4961d42a6e7
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-08-25 01:26:32 +0700

    fix: calculate point

--- src/app/absensi/absensiService.ts
+++ src/app/absensi/absensiService.ts
@@ -52,10 +52,10 @@ export const createAbsensiSubAcaraService = async ({ subAcaraId, image, coordina
     const absensiDate = new Date(FormatIDTime(absensi.createdAt))
 
     // Periksa jika absensi dilakukan 3 jam lebih awal
-    const timeDifference = (absensiDate.getTime() - targetTime.getTime()) / (1000 * 60 * 60) // Selisih dalam jam
+    const timeDifference = (targetTime.getTime() - absensiDate.getTime()) / (1000 * 60 * 60) // Selisih dalam jam
     let points = 20 // Poin default
 
-    if (timeDifference <= 1) {
+    if (timeDifference >= 0 && timeDifference <= 1) {
         points = 100 // Poin untuk absen 3 jam lebih awal
     }
     await addPoint(absensi.id, userId, points)
