a8402e4893b50653c6307c301adc256d24363e0b
Muhammad Fikri <muhfikriantoaji@gmail.com>
2024-09-02 22:06:45 +0700

    fix: logout

--- src/app/authentication/authRoute.ts
+++ src/app/authentication/authRoute.ts
@@ -4,6 +4,7 @@ import { VerifyToken } from "../../middleware/verifyToken";
 import { CatchWrapper } from "../../utils/CatchWrapper";
 import { checkToken, forgotPasswordController, loginAdminController, loginController, logoutController, registerController, requestOtpController, validateOtpController } from "./authController";
 import { forgotPasswordSchema, loginSchema, registerSchema, requestOtpSchema, validateOtpSchema } from "./authRequest";
+import { VerifyTokenWithoutExp } from "../../middleware/verifyTokenWithoutExp";
 
 const route = Router()
 
@@ -13,7 +14,7 @@ route.post("/admin/login", validateRequest(loginSchema), CatchWrapper(loginAdmin
 route.post('/otp', validateRequest(requestOtpSchema), CatchWrapper(requestOtpController))
 route.post('/otp/validate', validateRequest(validateOtpSchema), CatchWrapper(validateOtpController))
 route.put('/forgot-password', validateRequest(forgotPasswordSchema), CatchWrapper(forgotPasswordController))
-route.get('/logout', VerifyToken, CatchWrapper(logoutController))
+route.get('/logout', VerifyTokenWithoutExp, CatchWrapper(logoutController))
 route.get('/token', VerifyToken, CatchWrapper(checkToken))
 
 export default route
