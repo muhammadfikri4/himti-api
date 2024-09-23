f288edf4ccaa956c0a6ae3f4bd4f01563ae2211a
muhammadfikri4 <muhfikriantoaji@gmail.com>
2024-09-23 23:05:57 +0700

    Fix Path Struktural

--- src/app/struktural/strukturalMapper.ts
+++ src/app/struktural/strukturalMapper.ts
@@ -1,8 +1,10 @@
 import { Structural } from "@prisma/client"
+import { ImagePath } from "../../utils/ImagePath"
 
 export const structuralMapper = (data: Structural[]) => {
     return data.map(item => ({
         ...item,
-        jabatan: item.jabatan?.replace(/_/g, ' ')
+        jabatan: item.jabatan?.replace(/_/g, ' '),
+        image: item.image.includes('https') ? item.image : ImagePath(`struktural/${item.image}`)
     }))
 }
