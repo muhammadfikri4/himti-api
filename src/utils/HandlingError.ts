a460ba79c176a99d14aa73dcfa0bf3310dc94cb8
muhammadfikri4 <muhfikriantoaji@gmail.com>
2024-10-01 22:09:24 +0700

    fix: add error

--- src/utils/HandlingError.ts
+++ src/utils/HandlingError.ts
@@ -1,13 +1,25 @@
-import { NextFunction, type Request, type Response } from "express";
+import { type Request, type Response } from "express";
 import { MESSAGE_CODE } from "./ErrorCode";
 import { HandleResponse } from "./HandleResponse";
 import { ErrorApp } from "./HttpError";
 import { MESSAGES } from "./Messages";
 
-export const HandlingError = (err: ErrorApp | Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof ErrorApp) {
-        return HandleResponse(res, err.statusCode, err.code, err.message)
-    }
-    console.log(err)
-    return HandleResponse(res, 500, MESSAGE_CODE.INTERNAL_SERVER_ERROR, MESSAGES.ERROR.SERVER_ERROR.INTERNAL_SERVER_ERROR)
-}
+export const HandlingError = (
+  err: ErrorApp | Error,
+  req: Request,
+  res: Response
+) => {
+  if (err instanceof ErrorApp) {
+    console.log({
+      err,
+    });
+    return HandleResponse(res, err.statusCode, err.code, err.message);
+  }
+  console.log({ err });
+  return HandleResponse(
+    res,
+    500,
+    MESSAGE_CODE.INTERNAL_SERVER_ERROR,
+    MESSAGES.ERROR.SERVER_ERROR.INTERNAL_SERVER_ERROR
+  );
+};
